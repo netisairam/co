f8be9b3656925731c4c2182a9751730173f88fda
netisairam <sairam.neti@cartrade.com>
2018-06-27 17:30:56 +0530

    added login page

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,8 +1,14 @@
 <?php
 class Home extends CI_Controller{
 	public function index(){
-		$title = "login";
-		$this->load->view('car/header.php');
+		$data = array(
+   		 		'title' => 'Login',
+  		  		'heading' => 'My Heading',
+  		  		'message' => 'My Message'
+			);
+
+
+		$this->load->view('car/header.php', $data);
 		$this->load->view('car/login.php');
 		$this->load->view('car/footer.php');
 	}
